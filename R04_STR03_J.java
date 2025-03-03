e2c21132d3dcd3963415a64c22d16cc650dafcbc
no655256 <no655256@ucf.edu>
2025-03-02 23:43:46 -0500

    Update R04_STR03_J.java
    added rule name

--- R04_STR03_J.java
+++ R04_STR03_J.java
@@ -1,3 +1,4 @@
+//Rule 04 Characters and Strings (STR) STR03-J. Do not encode noncharacter data as a string
 BigInteger x = new BigInteger("530500452766");
 String s = x.toString();  // Valid character data
 byte[] byteArray = s.getBytes();
