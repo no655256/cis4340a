2198077cdc89271d1de74d288f9c877a74fe4171
no655256 <no655256@ucf.edu>
2025-03-02 23:24:33 -0500

    Update R08_VNA04_J.java
    Changed from the noncompliant code to the Compliant solution

--- R08_VNA04_J.java
+++ R08_VNA04_J.java
@@ -1,49 +1,73 @@
 final class USCurrency {
-  // Change requested, denomination (optional fields)
-  private int quarters = 0;
-  private int dimes = 0;
-  private int nickels = 0;
-  private int pennies = 0;
+  private final int quarters;
+  private final int dimes;
+  private final int nickels;
+  private final int pennies;
  
-  public USCurrency() {}
+  public USCurrency(Builder builder) {
+    this.quarters = builder.quarters;
+    this.dimes = builder.dimes;
+    this.nickels = builder.nickels;
+    this.pennies = builder.pennies;
+  }
  
-  // Setter methods
-  public USCurrency setQuarters(int quantity) {
-    quarters = quantity;
-    return this;
-  }
-  public USCurrency setDimes(int quantity) {
-    dimes = quantity;
-    return this;
-  }
-  public USCurrency setNickels(int quantity) {
-    nickels = quantity;
-    return this;
-  }
-  public USCurrency setPennies(int quantity) {
-    pennies = quantity;
-    return this;
+  // Static class member
+  public static class Builder {
+    private int quarters = 0;
+    private int dimes = 0;
+    private int nickels = 0;
+    private int pennies = 0;
+ 
+    public static Builder newInstance() {
+      return new Builder();
+    }
+ 
+    private Builder() {}
+ 
+    // Setter methods
+    public Builder setQuarters(int quantity) {
+      this.quarters = quantity;
+      return this;
+    }
+    public Builder setDimes(int quantity) {
+      this.dimes = quantity;
+      return this;
+    }
+    public Builder setNickels(int quantity) {
+      this.nickels = quantity;
+      return this;
+    }
+    public Builder setPennies(int quantity) {
+      this.pennies = quantity;
+      return this;
+    }
+ 
+    public USCurrency build() {
+      return new USCurrency(this);
+    }
   }
 }
  
 // Client code:
-class ExampleClientCode {
+class ExampleClientCode  {
  
-  private final USCurrency currency = new USCurrency();
+  private volatile USCurrency currency;
   // ...
  
   public ExampleClientCode() {
  
     Thread t1 = new Thread(new Runnable() {
         @Override public void run() {
-          currency.setQuarters(1).setDimes(1);
+          currency = USCurrency.Builder.newInstance().
+                         setQuarters(1).setDimes(1).build();
         }
     });
     t1.start();
  
     Thread t2 = new Thread(new Runnable() {
         @Override public void run() {
-          currency.setQuarters(2).setDimes(2);
+          currency = USCurrency.Builder.newInstance().
+                         setQuarters(2).setDimes(2).build();
         }
     });
     t2.start();
