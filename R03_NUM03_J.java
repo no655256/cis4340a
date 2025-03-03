a0c75e2db98d7ab606989d0ffb08babcb84a5be9
no655256 <no655256@ucf.edu>
2025-03-02 23:43:02 -0500

    Update R03_NUM03_J.java
    added rule name

--- R03_NUM03_J.java
+++ R03_NUM03_J.java
@@ -1,3 +1,4 @@
+//Rule 03 Numeric Types and Operations (NUM) NUM03-J. Use integer types that can fully represent the possible range of unsigned data
 
 public static long getInteger(DataInputStream is) throws IOException {
   return is.readInt() & 0xFFFFFFFFL; // Mask with 32 one-bits
